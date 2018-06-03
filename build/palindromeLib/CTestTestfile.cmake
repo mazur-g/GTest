ae88db240916fb968fddd208d0a155a93db976d0
mazur-g <maz.gn1@gmail.com>
2018-06-03 19:31:52 +0200

    Test commit

--- build/palindromeLib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ja2/studia/ADV/build_systems/Exercises/task3/palindromeLib
-# Build directory: /home/ja2/studia/ADV/build_systems/Exercises/task3/build/palindromeLib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
