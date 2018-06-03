3439722d0386249de1b283f176a9352a28f43891
mazur-g <maz.gn1@gmail.com>
2018-06-03 20:07:53 +0200

    Test commit

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ja2/studia/ADV/build_systems/Exercises/task4
-# Build directory: /home/ja2/studia/ADV/build_systems/Exercises/task4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("palindromeLib")
-subdirs("palindromeTests")
--- build/palindromeTests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ja2/studia/ADV/build_systems/Exercises/task4/palindromeTests
-# Build directory: /home/ja2/studia/ADV/build_systems/Exercises/task4/build/palindromeTests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(PalindromeTest "PalindromeTest")
